7b3408f72807abf87cc6f455519e99a6fa359393
lvzl <lvzl@tiancom.com>
2026-05-24 03:54:09 +0800

    feat:隐藏高级配置中的跳过表达式/多实例/异步等配置项

--- src/views/ProcessDesigner/Panel/BaseActivity/Advanced.vue
+++ src/views/ProcessDesigner/Panel/BaseActivity/Advanced.vue
@@ -1,19 +1,19 @@
 <script setup lang="ts">
-import { useCustomRef } from '@/views/ProcessDesigner/utils/ElementUtil.ts'
+// import { useCustomRef } from '@/views/ProcessDesigner/utils/ElementUtil.ts'
 import MultiInstance from '../MultiInstance/index.vue'
 import Document from './Document.vue'
 import Async from './Async.vue'
 import { useBpmnContextService } from '@/hooks/useService.ts'
 import { computed } from 'vue'
 import { getBusinessObject } from 'bpmn-js/lib/util/ModelUtil'
-import juelSupport from '@/components/CodemirrorEditor/language/juel'
-import Codemirror from '@/components/CodemirrorEditor/index.vue'
+// import juelSupport from '@/components/CodemirrorEditor/language/juel'
+// import Codemirror from '@/components/CodemirrorEditor/index.vue'
 
 defineOptions({
   name: 'Advanced',
 })
 const { selectedElement } = useBpmnContextService()
-const skipExpression = useCustomRef('skipExpression')
+// const skipExpression = useCustomRef('skipExpression')
 const propertiesByName = computed(() => {
   const businessObject = getBusinessObject(selectedElement)
   return businessObject?.$descriptor?.propertiesByName || {}
@@ -22,7 +22,7 @@ const propertiesByName = computed(() => {
 
 <template>
   <el-collapse-item name="arg2" title="高级">
-    <el-form-item label="跳过表达式" v-if="propertiesByName['skipExpression']">
+    <!-- <el-form-item label="跳过表达式" v-if="propertiesByName['skipExpression']">
       <Codemirror
         no-wrap
         :max-rows="5"
@@ -31,10 +31,10 @@ const propertiesByName = computed(() => {
         :extensions="[juelSupport()]"
         v-model="skipExpression"
       />
-      <!--      <el-input v-model="skipExpression" clearable placeholder="请输入跳过表达式" />-->
-    </el-form-item>
-    <MultiInstance v-if="propertiesByName['loopCharacteristics']" />
-    <Async v-if="propertiesByName['async']" />
+      <el-input v-model="skipExpression" clearable placeholder="请输入跳过表达式" />
+    </el-form-item> -->
+    <!-- <MultiInstance v-if="propertiesByName['loopCharacteristics']" /> -->
+    <!-- <Async v-if="propertiesByName['async']" /> -->
     <Document />
   </el-collapse-item>
 </template>
